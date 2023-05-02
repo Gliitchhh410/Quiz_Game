e1567611059853d0384125b95c94f10e56d98f8d
Gliitchhh410 <ahmedmounir532@gmail.com>
2023-05-02 15:55:50 +0300

    Adding Score system

--- Quiz_game.py
+++ Quiz_game.py
@@ -1,3 +1,14 @@
+score = 0
+def right(num):
+    num += 1
+    print("Correct!!")
+    print("Score: " + str(num))
+    return num
+
+def wrong():
+    print("Wrong!!")
+    quit()
+
 print("Welcome to my computer quiz!")
 
 playing = input ("Do You Want To Play(y or n)? ")
@@ -9,24 +20,28 @@ print("let's get to the quiz!!")
 
 
 answer = input("what does CPU stand for? ")
-if answer.lower() == "centeral processing unit" :
-    print("Correct!!")
+if answer.lower() == "central processing unit" :
+    score = right(score)
 else:
-    print("Wrong!!")
+    wrong()
 
 
 answer = input("what does GPU stand for? ")
 if answer.lower() == "graphic processing unit":
-    print("Correct!!")
+    score = right(score)
 else:
-    print("Wrong!!")
+    wrong()
 
 
 answer = input("what does RAM stand for? ")
 if answer.lower() == "random access memory" :
-    print("Correct!!")
+    score = right(score)
 else:
-    print("Wrong!!")
+    wrong()
 
-print("Game Over")
+print("YOU WON!!!")
 quit()
+
+
+
+
