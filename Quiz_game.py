7a03e45866c5e1728ba7200bac2d06375bea51ac
Gliitchhh410 <ahmedmounir532@gmail.com>
2023-05-02 15:25:32 +0300

    Adding .lower() to lower all inputs

--- Quiz_game.py
+++ Quiz_game.py
@@ -2,28 +2,28 @@ print("Welcome to my computer quiz!")
 
 playing = input ("Do You Want To Play(y or n)? ")
 
-if playing != "y":
+if playing.lower() != "y":
     quit()
 
 print("let's get to the quiz!!")
 
 
 answer = input("what does CPU stand for? ")
-if answer == "center processing unit" or answer == "Central processing unit" or answer == "central proccessing unit":
+if answer.lower() == "centeral processing unit" :
     print("Correct!!")
 else:
     print("Wrong!!")
 
 
 answer = input("what does GPU stand for? ")
-if answer == "Graphic processing unit" or answer == "graphic processing unit" or answer == "graphic proccessing unit":
+if answer.lower() == "graphic processing unit":
     print("Correct!!")
 else:
     print("Wrong!!")
 
 
 answer = input("what does RAM stand for? ")
-if answer == "Random access memory" or answer == "random access memory":
+if answer.lower() == "random access memory" :
     print("Correct!!")
 else:
     print("Wrong!!")
